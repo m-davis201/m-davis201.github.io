aabca5ac5b1053a9af9f4bf914afde45f91cf972
m-davis201 <118855409+m-davis201@users.noreply.github.com>
2023-01-22 14:05:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const txt1 = document.getElementById('tbuser');
 const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
 const out1 = document.getElementById('output1');
-const itemList = [];
+const ol = document.getElementById("first-ol");
+const itemArray = [];
 
-	function fun1(){
-		itemList.push(txt1.value);
-		out1.innerHTML = itemList;
-    }
 
-	btn1.addEventListener('click', fun1);
-    txt1.addEventListener('keyup', (e) => {
-        if(e.keyCode === 13){
-            fun1();
-        }
+function fun1(){
+	itemArray.push(txt1.value);
+    itemArray.forEach(function(e){
+        const li =
+        document.createElement('li');
+        li.innerText = e;
+        ol.append(li)
     })
+}
+
+
+
+
+btn1.addEventListener('click', fun1);
+txt1.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13){
+        fun1();
+    }
+})
+
+   
